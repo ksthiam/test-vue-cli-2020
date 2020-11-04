e2cb31dc4899a9dd015eddd3b29c0b09344cc25b
ksthiam <khaly_sileye.thiam@edu.univ-fcomte.fr>
2020-11-04 10:12:02 +0100

    ajout data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button>Open</button>
+
+    <div class="dialog-window" v-show="visible">
+      <header>
+        <h1>Le titre</h1>
+      </header>
+      <div class="main">
+        Contenu
+      </div>
+      <footer>
+        <button >Close</button>
+      </footer>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+
+  },
+  data: function (){
+    return {
+      visible: false,
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

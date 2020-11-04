10b99a530cbe1c4f04d2ff5e511b1044d8c434ae
ksthiam <khaly_sileye.thiam@edu.univ-fcomte.fr>
2020-11-04 18:44:08 +0100

    Commit : backdrop pour SimpleDialog

--- src/components/SimpleDialog.vue
+++ src/components/SimpleDialog.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="app">
+    <div class ="dialog-parent">
 
-
+        <div class="dialog-backdrop" v-show="visible"></div>
         <div class="dialog-window" v-show="visible">
             <header>
                 <h1>{{titre}}</h1>
@@ -44,6 +44,28 @@
     }
 </script>
 
-<style>
+<style scoped>
+    .dialog-window {
+        position: relative;
+        pointer-events: auto;
+    }
+    .dialog-parent {
+        pointer-events: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .dialog-parent,
+    .dialog-backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+    .dialog-backdrop {
+        pointer-events: auto;
+        background-color: rgba(255, 30, 80, 0.5);
+    }
 
 </style>

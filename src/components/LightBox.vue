15124e414a8ac050c94ce299a737924a7b44ecb9
ksthiam <khaly_sileye.thiam@edu.univ-fcomte.fr>
2020-11-21 12:38:42 +0100

    Commit : niniature comme bouton

--- src/components/LightBox.vue
+++ src/components/LightBox.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="light-box">
     <ul>
-      <li><img src="images/thumbnails/animals-1.jpeg" data-full-img="images/animals-1.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-2.jpeg" data-full-img="images/animals-2.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-3.jpeg" data-full-img="images/animals-3.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-4.jpeg" data-full-img="images/animals-4.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-5.jpeg" data-full-img="images/animals-5.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-6.jpeg" data-full-img="images/animals-6.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-7.jpeg" data-full-img="images/animals-7.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-8.jpeg" data-full-img="images/animals-8.jpeg" alt=""/></li>
-      <li><img src="images/thumbnails/animals-9.jpeg" data-full-img="images/animals-9.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-1.jpeg" data-full-img="images/animals-1.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-2.jpeg" data-full-img="images/animals-2.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-3.jpeg" data-full-img="images/animals-3.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-4.jpeg" data-full-img="images/animals-4.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-5.jpeg" data-full-img="images/animals-5.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-6.jpeg" data-full-img="images/animals-6.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-7.jpeg" data-full-img="images/animals-7.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-8.jpeg" data-full-img="images/animals-8.jpeg" alt=""/></li>
+      <li><img @click="$refs.dialog.show()" src="images/thumbnails/animals-9.jpeg" data-full-img="images/animals-9.jpeg" alt=""/></li>
     </ul>
 
     <button @click="$refs.dialog.show()">Open</button>

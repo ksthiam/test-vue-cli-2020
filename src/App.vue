8251236602362683d262c9ac97ff3341955c5c93
ksthiam <khaly_sileye.thiam@edu.univ-fcomte.fr>
2020-11-25 09:31:31 +0100

    Commit : ajout filtre sur type==="sauvage"

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Bonjour</h1>
-    <light-box :images-data="ListeImages" />
+    <light-box :images-data="imagesDataFiltres" />
   </div>
 </template>
 
@@ -20,62 +20,73 @@ export default {
           "dataFullImg": "images/animals-1.jpeg",
           "title":"Gorille",
           "alt":"une jolie image",
+          "type": "sauvage"
+
         },
         {
           "src": "images/thumbnails/animals-2.jpeg",
           "dataFullImg": "images/animals-2.jpeg",
           "title":"banbi",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "sauvage"
 
         },
         {
           "src": "images/thumbnails/animals-3.jpeg",
           "dataFullImg": "images/animals-3.jpeg",
           "title":"cochon",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "domestique"
         },
         {
           "src": "images/thumbnails/animals-4.jpeg",
           "dataFullImg": "images/animals-4.jpeg",
           "title":"Rhinocéroce",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "sauvage"
         },
         {
           "src": "images/thumbnails/animals-5.jpeg",
           "dataFullImg": "images/animals-5.jpeg",
           "title":"Chat",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "domestique"
         },
         {
           "src": "images/thumbnails/animals-6.jpeg",
           "dataFullImg": "images/animals-6.jpeg",
           "title":"Chien",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "domestique"
         },
         {
           "src": "images/thumbnails/animals-7.jpeg",
           "dataFullImg": "images/animals-7.jpeg",
           "title":"Guepard",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "sauvage",
+
         },
         {
           "src": "images/thumbnails/animals-8.jpeg",
           "dataFullImg": "images/animals-8.jpeg",
           "title":"Ziraff",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "sauvage"
         },
         {
           "src": "images/thumbnails/animals-9.jpeg",
           "dataFullImg": "images/animals-9.jpeg",
           "title":"Chien",
-          "alt":"une jolie image"
+          "alt":"une jolie image",
+          "type": "domestique"
         }
       ]
     }
   },
   computed: {
     imagesDataFiltres() {
-      return [...this.ListeImages];
+      return this.ListeImages.filter(({type})=>type==="sauvage");
 
     }
   },
